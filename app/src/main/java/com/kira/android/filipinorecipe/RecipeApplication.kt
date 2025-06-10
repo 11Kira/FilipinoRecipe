f47501cc77f1a88f61644c254e635077514158aa
k.i.r.a. <kiravillagracia@yahoo.com>
2025-06-10 13:50:46 +0800

    added application class

--- app/src/main/java/com/kira/android/filipinorecipe/RecipeApplication.kt
+++ app/src/main/java/com/kira/android/filipinorecipe/RecipeApplication.kt
@@ -1,4 +1,7 @@
 package com.kira.android.filipinorecipe
 
-class RecipeApplication {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RecipeApplication: Application()
