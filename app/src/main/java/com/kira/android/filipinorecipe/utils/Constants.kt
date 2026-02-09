af7a9be5bace1a049f13c38a3c59b92f5e428b53
k.i.r.a. <kiravillagracia@yahoo.com>
2026-02-09 16:03:50 +0800

    updated port and ip config to connect to local server

--- app/src/main/java/com/kira/android/filipinorecipe/utils/Constants.kt
+++ app/src/main/java/com/kira/android/filipinorecipe/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.kira.android.filipinorecipe.utils
 
 object Constants {
-    const val BASE_URL = "http://192.168.100.67:8080/api/"
+    const val BASE_URL = "http://192.168.68.140:8080/api/"
 
     const val CONNECT_TIMEOUT = 60L
     const val READ_TIMEOUT = 60L
