c2cae96125bdcd1b2e9e660b151969e7738662be
k.i.r.a. <kiravillagracia@yahoo.com>
2026-04-16 10:57:54 +0800

    converted composables as reusable components

--- app/src/main/java/com/kira/android/filipinorecipe/features/recipes/favorites/FavoriteRecipeListScreen.kt
+++ app/src/main/java/com/kira/android/filipinorecipe/features/recipes/favorites/FavoriteRecipeListScreen.kt
@@ -2,29 +2,29 @@ package com.kira.android.filipinorecipe.features.recipes.favorites
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import androidx.navigation.NavController
 import com.kira.android.filipinorecipe.utils.ColorUtils
 
 @Composable
 fun FavoriteRecipeListScreen(
-    navController: NavController,
-    viewModel: FavoriteRecipeListViewModel = hiltViewModel()
+    viewModel: FavoriteRecipeListViewModel = hiltViewModel(),
+    contentPadding: PaddingValues,
+    onItemClick: (String) -> Unit,
 ) {
     PopulateFavoriteRecipeListScreen(
         viewModel = viewModel,
-        navController = navController
-
+        onItemClick = onItemClick,
     )
 }
 
 @Composable
 fun PopulateFavoriteRecipeListScreen(
     viewModel: FavoriteRecipeListViewModel,
-    navController: NavController
+    onItemClick: (String) -> Unit,
 ) {
     Box(
         modifier = Modifier
